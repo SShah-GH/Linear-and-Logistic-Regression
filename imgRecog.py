58ee5a101bd404da991889a1e436501f53957fe9
Satva <satvabshah@gmail.com>
2020-12-20 16:19:45 -0800

    Adding ski-kit learn steps

--- imgRecog.py
+++ imgRecog.py
@@ -1,6 +1,27 @@
 import numpy as np 
 import pandas as pd 
 
-#Testing Comment
+import pandas as pd
+dataset = pd.read_csv('dataset_iris.csv')
+dataset.head()
 
-# More fantastical testing
+x = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(multi_class='ovr', random_state = 0)
+classifier.fit(x_train, y_train)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+predictions = classifier.predict(x_test)
+cm = confusion_matrix(predictions, y_test)
+print(cm)
+accuracy_score(predictions, y_test)
